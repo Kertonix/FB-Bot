ae605e507b603664d487eff9b6879c036fcbd427
Mateusz Bagilcz <mbagi@softserveinc.com>
2019-12-14 13:56:25 +0100

    Updated mock

--- mock.js
+++ mock.js
@@ -1,12 +1,123 @@
 export default mock = [
   {
+    name: 'American Burger',
     cousine: 'burger',
     pricePoint: 'medium',
     coords: {
-      long: '',
-      lat: '',
+      long: '20.985802',
+      lat: '52.232821',
     },
-    rating: '4.7',
-    vege: true
-  }
+    rating: '3.2',
+    vege: false,
+  },
+  {
+    name: 'Mariano Italiano',
+    cousine: 'italian',
+    pricePoint: 'low',
+    coords: {
+      long: '20.985781',
+      lat: '52.232220',
+    },
+    rating: '4.2',
+    vege: true,
+  },
+  {
+    name: 'Parmeggiano',
+    cousine: 'italian',
+    pricePoint: 'high',
+    coords: {
+      long: '20.992004',
+      lat: '52.231655',
+    },
+    rating: '4.9',
+    vege: false,
+  },
+  {
+    name: 'U Krystyny',
+    cousine: 'polish',
+    pricePoint: 'medium',
+    coords: {
+      long: '20.997916',
+      lat: '52.236038',
+    },
+    rating: '3.7',
+    vege: true,
+  },
+  {
+    name: 'McDonalds',
+    cousine: 'burger',
+    pricePoint: 'low',
+    coords: {
+      long: '20.999686',
+      lat: '52.231537',
+    },
+    rating: '4.3',
+    vege: true,
+  },
+  {
+    name: 'Kotlet Mielony',
+    cousine: 'polish',
+    pricePoint: 'low',
+    coords: {
+      long: '21.012045',
+      lat: '52.229567',
+    },
+    rating: '3.5',
+    vege: true,
+  },
+  {
+    name: 'Mario&Luiggi',
+    cousine: 'itaian',
+    pricePoint: 'high',
+    coords: {
+      long: '21.013887',
+      lat: '52.234887',
+    },
+    rating: '4.2',
+    vege: true,
+  },
+  {
+    name: 'Kura Zmielona',
+    cousine: 'burger',
+    pricePoint: 'medium',
+    coords: {
+      long: '21.004166',
+      lat: '52.241587',
+    },
+    rating: '3.3',
+    vege: true,
+  },
+  {
+    name: 'BurBurger',
+    cousine: 'burger',
+    pricePoint: 'low',
+    coords: {
+      long: '20.999538',
+      lat: '52.220873',
+    },
+    rating: '3.6',
+    vege: false,
+  },
+  {
+    name: 'Rosół i Familiada',
+    cousine: 'polish',
+    pricePoint: 'low',
+    coords: {
+      long: '21.016659',
+      lat: '52.213820',
+    },
+    rating: '3.8',
+    vege: true,
+  },
+  {
+    name: 'Bulka',
+    cousine: 'burger',
+    pricePoint: 'medium',
+    coords: {
+      long: '21.041418',
+      lat: '52.240875',
+    },
+    rating: '4.1',
+    vege: false,
+  },
 ];
